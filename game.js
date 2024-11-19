80a6bb4e2d0147f9cc5cda902e61e026508382f8
Sencoool <s6506021610037@email.kmutnb.ac.th>
2024-11-19 18:51:41 +0700

    Testing snake draw

--- game.js
+++ game.js
@@ -1,1 +1,32 @@
-console.log("test");
+const gameBoard = document.getElementById("game-board");
+
+// Game variable
+let snake = [{ x: 10, y: 10 }]; // Snake starting position
+
+// console.log(gameBoard);
+
+function draw() {
+  gameBoard.innerHTML = "";
+  drawSnake();
+}
+
+function drawSnake() {
+  snake.forEach((segment) => {
+    const snakeElement = createGameElement("div", "snake");
+    setPosition(snakeElement, segment);
+    gameBoard.appendChild(snakeElement);
+  });
+}
+
+function createGameElement(tag, className) {
+  const element = document.createElement(tag);
+  element.className = className;
+  return element;
+}
+
+function setPosition(element, position) {
+  element.style.gridColumn = position.x;
+  element.style.girdRow = position.y;
+}
+
+draw();
